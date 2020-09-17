e7eb11953e009e3117a93def57aff319da6f1a69
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-17 13:17:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,7 @@ def square_array(array)
   new_numbers = []
   count = 0 
   while count < array.length do
-    array[count] do |numbers|
-    new_numbers << (numbers*numbers)
+    new_numbers << numbers*numbers
     count += 1
   end
     return new_numbers
